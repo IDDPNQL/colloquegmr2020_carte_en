88231ceb52b85009bdcd7754ac07d0c1ab144ffa
IDDPNQL <61841279+IDDPNQL@users.noreply.github.com>
2020-11-19 15:50:52 -0500

    Update custom-scripts.js

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -1,4 +1,4 @@
-require(["dojo/topic"], function(topic) {
+require(["dojo/topic","dijit/Dialog"], function(topic, Dialog) {
 	/*
 	* Custom Javascript to be executed while the application is initializing goes here
 	*/
